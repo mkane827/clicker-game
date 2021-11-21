e472f7b7b464adeaa28c9d15b258bc60a40d562c
Jenny Kay Pollock <jenniferkaypollock@gmail.com>
2021-11-20 16:12:24 -0800

    Update activityTracker.js (#2)
    * Update activityTracker.js
    
    New activity tracker items based on lines of code generated and a one time activity to hire sales.
    
    * Update activityTracker.js
    
    * fixing time
    
    * moving stuff
    
    Co-authored-by: Malcolm Kane <mkane827@gmail.com>

--- src/activityTracker.js
+++ src/activityTracker.js
@@ -20,7 +20,7 @@ export function oneTimeActivity(conditionFn, activity) {
       addActivity(activity);
       clearInterval(interval);
     }
-  }, TICK_SPEED);
+  }, 1);
 }
 
 export function linesOfCodeActivity(numLines, activity) {
@@ -28,21 +28,47 @@ export function linesOfCodeActivity(numLines, activity) {
 }
 
 linesOfCodeActivity(10, "No one knows about your startup idea, just you");
-linesOfCodeActivity(15, "A few close friends say they like your idea");
 linesOfCodeActivity(
-  250,
+  15,
+  "A few close friends say they like your idea you can't tell if they mean it or if they are just being nice"
+);
+linesOfCodeActivity(30, "Your parents do not understand your idea");
+linesOfCodeActivity(
+  40,
   "Coding all night pays off, your MVP is ready to share!"
 );
-linesOfCodeActivity(300, "Give your first demo");
+linesOfCodeActivity(45, "Give your first demo");
+linesOfCodeActivity(50, "Your demo didn't go as planned");
 linesOfCodeActivity(
-  325,
+  60,
   "A few people are talking your idea is getting some traction"
 );
 linesOfCodeActivity(
-  400,
+  70,
   "Your scrum board is overflowing with tasks, time to get some cofounders"
 );
+linesOfCodeActivity(80, "Apply to the top incubators");
+linesOfCodeActivity(105, "Get rejected from the top incubators");
+linesOfCodeActivity(110, "Perfect your demo and pitch");
+linesOfCodeActivity(
+  130,
+  "Lucky break a spot opened in your second choice incubator "
+);
+linesOfCodeActivity(145, "Search for product market fit");
+linesOfCodeActivity(165, "Congradulations you have your first customer");
+linesOfCodeActivity(
+  178,
+  "Look for more customers to confirm product market fit"
+);
+linesOfCodeActivity(
+  195,
+  "You’re having trouble finding product market fit lines of code now generate 50% less revenue"
+);
 oneTimeActivity(
-  () => state.linesOfCode > 400 && state.money >= 5000,
+  () => state.linesOfCode > 125 && state.money >= 5000,
   "It's time to make your first hire, pick wisely"
+);
+oneTimeActivity(
+  () => state.linesOfCode > 220 && state.money >= 10000,
+  "You need customers go hire a head of sales"
 );
