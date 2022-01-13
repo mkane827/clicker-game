93371ddd7bfb47b045767b790677ab7df718df13
Malcolm Kane <mkane827@gmail.com>
2022-01-13 10:54:50 -0800

    prod build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,6 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: '/clicker.malcolmkane.com'
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
